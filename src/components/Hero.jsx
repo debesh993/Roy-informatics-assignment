b4348c6f28fd7c4b1718153855fd618b5645efa5
Debesh Dinda <debeshdinda993@gmail.com>
2026-02-15 09:19:45 +0530

    Some design changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,34 +3,45 @@ import { Autoplay, Pagination, EffectFade } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/effect-fade";
-import image1 from '../assets/admin_hero_images/20220924072451_banner1.webp'
-import image2 from '../assets/admin_hero_images/20220924072606_banner4.webp'
+import image1 from "../assets/admin_hero_images/20220924072451_banner1.webp";
+import image11 from "../assets/admin_hero_images/20220924072451_banner.webp";
 
-import image3 from '../assets/admin_hero_images/202306050147251819.jpg'
-import image4 from '../assets/admin_hero_images/202506220226159388.jpg'
-import image5 from '../assets/admin_hero_images/202506220234343515.jpg'
+import image2 from "../assets/admin_hero_images/20220924072606_banner.webp";
+import image21 from "../assets/admin_hero_images/20220924072606_banner4.webp";
+
+import image3 from "../assets/admin_hero_images/202306050147251819.jpg";
+import image31 from "../assets/admin_hero_images/20230605014725181.jpg";
+
+import image4 from "../assets/admin_hero_images/202506220226159388.jpg";
+import image5 from "../assets/admin_hero_images/202506220234343515.jpg";
+import image51 from "../assets/admin_hero_images/20250622023434351.jpg";
 
 const slides = [
   {
     id: 1,
-    image: image1,
+    imageSmall: image11,
+    imageLarge: image1,
   },
   {
     id: 2,
-    image: image2,
+    imageSmall: image21,
+    imageLarge: image2,
   },
   {
-    id:3,
-    image:image3
+    id: 3,
+    imageSmall: image31,
+    imageLarge: image3,
   },
   {
-    id:4,
-    image:image4
+    id: 4,
+    imageSmall: image4,
+    imageLarge: image4,
   },
   {
-    id:5,
-    image:image5
-  }
+    id: 5,
+    imageSmall: image51,
+    imageLarge: image5,
+  },
 ];
 
 const Hero = () => {
@@ -48,10 +59,11 @@ const Hero = () => {
           <SwiperSlide key={slide.id}>
             <div
               className="w-full h-screen bg-cover bg-center relative"
-              style={{ backgroundImage: `url(${slide.image})` }}
+              style={{
+                backgroundImage: `url(${window.innerWidth >= 1024 ? slide.imageLarge : slide.imageSmall})`,
+              }}
             >
               {/* Dark Overlay */}
-              
 
               {/* Content Container (if needed later) */}
               <div className="relative z-10 flex items-center justify-center h-full px-6">
@@ -66,4 +78,3 @@ const Hero = () => {
 };
 
 export default Hero;
-
